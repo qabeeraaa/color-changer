d49293f4a34ca3508240df44f74424d14201f98b
qabeeraaa <106308640+qabeeraaa@users.noreply.github.com>
2022-08-18 01:21:45 +0530

    Add files via upload

---
+++ appr.js
@@ -0,0 +1,23 @@
+
+
+const colors = getRandomNumber();
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function () {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = getRandomNumber();
+    color.textContent = getRandomNumber();
+})
+
+function getRandomNumber() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+
+    for (var i = 0; i < 10; i++) {
+        console.log(getRandomNumber());
+    }
+}
